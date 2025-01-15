61b1de655b2ccb8f256d8704f1f38f6a10492b05
Adnan Gujarati <93821999+Adnan4269@users.noreply.github.com>
2025-01-15 22:47:17 +0530

    Blinkit_Grocerystore.sql

--- Blinkit_Grocerystore.sql
+++ Blinkit_Grocerystore.sql
@@ -1,232 +1,267 @@
-create database blinkit;
-use blinkit;
+create database sky_sports;
+use sky_sports;
 show tables;
 
-#Q1 Import Data from table Grocery Sales using the provided CSV File.
-select max(item_mrp) from Grocery_Store;
+select * from skysports;
 
-#Q2 Write an SQL query to show all Item_Identifier
-select Item_Identifier from Grocery_Store;
+#1. UNIQUE team names
+select distinct(team)
+from skysports;
 
-#Q3 Write an SQL query to show count of total Item_Identifier
-select count(Item_Identifier) from Grocery_Store;
+#3.	 count of all team
+select count(team) 
+from skysports;
 
-#Q4 Write an SQL query to show maximum Item Weight.
-select max(Item_Weight) from Grocery_Store;
+#4. matches_played by each team
+select team, matches_played 
+from skysports;
 
-#Q5 Write an SQL query to show minimum Item Weight.
-select min(Item_Weight) from Grocery_Store;
+#5. team, percent of wins with respect to matches_played by each team and name the resulting column as wins_percent
+select team, 100.0*(wins/matches_played) as wins_percent 
+from skysports;
 
-#Q6 Write an SQL query to show average Item_Weight
-select avg(Item_Weight) from Grocery_Store;
+#6.	 which team has maximum goals_scored and how much
+SELECT team, goals_scored 
+FROM skysports
+WHERE goals_scored = (SELECT MAX(goals_scored) FROM skysports);
 
-#Q7 Write an SQL query to show count of Item_Fat_Content WHERE Item_Fat_Content is Low Fat.
-select count(Item_Fat_Content) from Grocery_Store where Item_Fat_Content = 'Low Fat';
+#7.	 percent of draws with respect to matches_played round of to 2 digits by each team
+select team, round(100.0*(draws/matches_played), 2) 
+from skysports;
 
-#Q8 Write an SQL query to show count of Item_Fat_Content WHERE Item_Fat_Content is Regular.
-select count(Item_Fat_Content) from Grocery_Store where Item_Fat_Content = 'Regular';
+#8.	 which team has minimum goals_scored and how much
+SELECT team, goals_scored 
+FROM skysports 
+WHERE goals_scored = (SELECT min(goals_scored) FROM skysports);
 
-#Q9 Write an SQL query to show maximum Item_MRP 
-select max(Item_MRP) from Grocery_Store; 
+#9.	 percent of losses with respect to matches_played by each team in ascending order by losses and name the resulting column as losses_percent
+select team, 100.0*(losses/matches_played) as losses_percent 
+from skysports 
+order by losses_percent;
 
-#Q10 Write an SQL query to show minimum Item_MRP
-select min(Item_MRP) from Grocery_Store; 
+#10.  the average goal_difference
+select avg(goal_difference) 
+from skysports;
 
-#Q11 Write an SQL query to show Item_Identifier , Item_Fat_Content ,Item_Type, Item_MRP whose Item_MRP is greater than 200.
-select Item_Identifier , Item_Fat_Content ,Item_Type,Item_MRP from Grocery_Store where Item_MRP>200;
+#11.  name of the team where points are 0
+select team, points 
+from skysports 
+where points=0;
 
-#Q12 Write an SQL query to show maximum Item_MRP WHERE Item_Fat_Content is Low Fat 
-select max(Item_MRP) from Grocery_Store where Item_Fat_Content = 'Low Fat';
+#12.  all data where expected_goal_scored is less than exp_goal_conceded
+select * 
+from skysports 
+where expected_goal_scored < exp_goal_conceded;
 
-#Q13 Write an SQL query to show minimum Item_MRP whose Item_Fat_Content is Low Fat 
-select min(Item_MRP) from Grocery_Store where Item_Fat_Content = 'Low Fat';
+#13.  data where exp_goal_difference is in between -0.5 and 0.5
+select * 
+from skysports 
+where exp_goal_difference between -0.5 and 0.5;
 
-#Q14 Write an SQL query to show ALL DATA WHERE item MRP is BETWEEN 50 to 100
-select * from Grocery_Store where Item_MRP between 50 and 100;
+#14.  all data in ascending order by exp_goal_difference_per_90 
+select * 
+from skysports 
+order by exp_goal_difference_per_90 asc;
 
-#Q15 Write an SQL query to show ALL UNIQUE value of Item_Fat_Content
-select distinct Item_Fat_Content from Grocery_Store;
+#15.  team which has maximum number of players_used
+select team, players_used 
+from skysports 
+where players_used = (SELECT max(players_used) FROM skysports);
 
-#Q16 Write an SQL query to show ALL UNIQUE value of  Item_Type 
-select distinct Item_Type from Grocery_Store;
+#16.  each team name and avg_age in ascending order by avg_age
+select team, avg_age 
+from skysports 
+order by avg_age;
 
-#Q17 Write an SQL query to show ALL DATA in descending ORDER by Item MRP 
-select * from Grocery_Store order by Item_MRP desc;
+#17.  average possession of teams
+select avg(possession) 
+from skysports;
 
-#Q18 Write an SQL query to show ALL DATA in ascending ORDER by Item_Outlet_Sales
-select * from Grocery_Store order by Item_Outlet_Sales asc;
+#18.  team which has played atleast 5 games
+select team, games 
+from skysports 
+where games>=5;
 
-#Q19 Write an SQL query to show ALL DATA in ascending by Item_Type 
-select * from Grocery_Store order by Item_Type asc;
+#19.  all data for which minutes is greater than 600
+select * 
+from skysports 
+where minutes>600;
 
-#Q20 Write an SQL query to show DATA of item_type dairy & Meat
-select * from Grocery_Store where Item_Type in ('Dairy','Meat');
+#20.  team, goals, assists in ascending order by goals
+select team, goals, assists 
+from skysports 
+order by goals;
 
-#Q21 Write an SQL query to show ALL UNIQUE value of Outlet_Size 
-select distinct Outlet_S
-ize from Grocery_Store;
+#21.  team, pens_made, pens_att in descending order by pens_made
+select team, pens_made, pens_att 
+from skysports 
+order by pens_made desc;
 
-#Q22 Write an SQL query to show ALL UNIQUE value of Outlet_Location_Type
-select distinct Outlet_Location_Type from Grocery_Store;
- 
-#Q23 Write an SQL query to show ALL UNIQUE value of Outlet_Type 
+#22.  team, cards_yellow, cards_red where cards_red is equal to 1 in ascending order by cards_yellow
+select team, cards_yellow, cards_red 
+from skysports 
+where cards_red=1 
+order by cards_yellow;
 
-select distinct Outlet_Type from Grocery_Store;
+#23.  team, goals_per90, assists_per90, goals_assists_per90 in descending order by goals_assists_per90
+select team, goals_per90, assists_per90, goals_assists_per90 
+from skysports 
+order by goals_assists_per90 desc;
 
-#Q24 Write an SQL query to show count of number of items by Item_Type  and order it in descending order
+#24.  team, goals_pens_per90, goals_assists_pens_per90 in ascending order by goals_assists_pens_per90
+select team, goals_pens_per90, goals_assists_pens_per90 
+from skysports 
+order by goals_assists_pens_per90;
 
-SELECT Item_Type , count(Item_Identifier)No_Of_Item  
-FROM Grocery_Store  
-GROUP BY Item_Type  
-ORDER BY No_Of_Item DESC;
+#25.  team, shots, shots_on_target, shots_on_target_pct where shots_on_target_pct is less than 30 in ascending order by shots_on_target_pct
+select  team, shots, shots_on_target, shots_on_target_pct 
+from skysports 
+where shots_on_target_pct<30 
+order by shots_on_target_pct;
 
-#Q25 Write an SQL query to show count of number of items by Outlet_Size and ordered it in ascending order 
-SELECT Outlet_Size , count(Item_Identifier)No_Of_Item  
-FROM Grocery_Store  
-GROUP BY Outlet_Size  
-ORDER BY No_Of_Item asc; 
+#26.  team, shots_per90, shots_on_target_per90 for team Belgium
+select team, shots_per90, shots_on_target_per90 
+from skysports 
+where team='Belgium';
 
-#Q26 Write an SQL query to show count of number of items by Outlet_Type  and ordered it in descending order.
-SELECT Outlet_Type , count(Item_Identifier) No_Of_Item  
-FROM Grocery_Store  
-GROUP BY Outlet_Type  
-ORDER BY No_Of_Item desc; 
+#27.  team, goals_per_shot, goals_per_shot_on_target, average_shot_distance in descending order by average_shot_distance
+select team, goals_per_shot, goals_per_shot_on_target, average_shot_distance 
+from skysports 
+order by average_shot_distance desc;
 
-#Q27 Write an SQL query to show count of items by Outlet_Location_Type and order it in descending order
-SELECT Outlet_Location_Type , count(Item_Identifier) No_Of_Item  
-FROM Grocery_Store  
-GROUP BY Outlet_Location_Type  
-ORDER BY No_Of_Item desc;
+#28.  team, errors, touches for which errors is 0 and touches is less than 1500
+select team, errors, touches  
+from skysports 
+where errors=0 and touches<1500;
 
-#Q28 Write an SQL query to show maximum MRP by Item_Type 
-SELECT  Item_Type, Max(Item_MRP) Max_MRP  
-FROM Grocery_Store  
-GROUP BY Item_Type; 
+#29.  team, fouls which has maximum number of fouls
+select team, max(fouls) 
+from skysports;
+#or
+select team, fouls 
+from skysports 
+where fouls=(select max(fouls) from skysports);
 
-#Q29 Write an SQL query to show minimum MRP by Item_Type  
-SELECT  Item_Type, min(Item_MRP)Min_MRP  
-FROM Grocery_Store  
-GROUP BY Item_Type;
+#30.  team, offisdes which has offsides less than 10 or greater than 20
+select team, offsides 
+from skysports 
+where offsides<10 or offsides>20;
 
-#Q30 Write an SQL query to show minimum MRP by Outlet_Establishment_Year and order it in descending order.
-SELECT  Outlet_Establishment_Year, min(Item_MRP) Min_MRP  
-FROM Grocery_Store  
-GROUP BY Outlet_Establishment_Year order by Min_MRP desc;
+#31.  team, aerials_won, aerials_lost, aerials_won_pct in descending order by aerials_won_pct
+select team, aerials_won, aerials_lost, aerials_won_pct 
+from skysports 
+order by aerials_won_pct desc;
 
-#Q31 Write an SQL query to show maximum MRP by Outlet_Establishment_Year and order it in descending order.
-SELECT  Outlet_Establishment_Year, Max(Item_MRP) Max_MRP  
-FROM Grocery_Store  
-GROUP BY Outlet_Establishment_Year order by Max_MRP desc;
+# group by
+#32.  number of teams each group has
+select `group`, count(team) 
+from skysports 
+group by `group`;
+
+#33.  team names group 6 has
+select team, `group` 
+from skysports 
+where `group`=6;
+
+#34.  Australia belongs to which group
+select team, `group` 
+from skysports
+where team='Australia';
+
+#35.  group, average wins by each group
+select `group`, avg(wins) 
+from skysports 
+group by `group`;
+
+#36.  group, maximum expected_goal_scored by each group in ascending order by expected_goal_scored  
+select gs.`group`, max(gs.expected_goal_scored) as max_exp_goal_scored 
+from skysports gs 
+group by gs.`group` 
+order by max_exp_goal_scored;
+
+#37.  group, minimum exp_goal_conceded by each group in descending order by exp_goal_conceded
+select gs.`group`, min(gs.exp_goal_conceded) as min_exp_goal_conceded 
+from skysports gs 
+group by gs.`group` 
+order by min_exp_goal_conceded desc;
+
+#38.  group, average exp_goal_difference_per_90 for each group in ascending order by exp_goal_difference_per_90
+select gs.`group`, avg(gs.exp_goal_difference_per_90) as avg_exp_goal_diff_per_90 
+from skysports gs 
+group by gs.`group` 
+order by avg_exp_goal_diff_per_90;
+
+#39.  which team has equal number of goals_scored and goals_against
+select team, goals_scored, goals_against 
+from skysports 
+where goals_scored = goals_against;
+
+#40.  which team has maximum players_used
+select team, players_used 
+from skysports 
+where players_used=(select max(players_used) from skysports);
+
+#41.  team, players_used, avg_age, games, minutes  where minutes less than 500 and greater than 200
+select team, players_used, avg_age, games, minutes 
+from skysports 
+where minutes < 500 and minutes > 200;
+
+#42.  all data of skysports in ascending order BY points
+select * 
+from skysports 
+order by points;
+
+#43.  ALL UNIQUE team in ascending order by team
+select * 
+from skysports2;
+
+#SQL Joins
+#44.  average avg_age of each group and arrange it in descending order by avg_age.
+select td.`group`, avg(gs.avg_age) as avg_group_age
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by avg_group_age desc;
+
+#45.  sum of fouls for each group and arrange it in ascending order by fouls.
+select td.`group`, sum(gs.fouls) as sum_fouls 
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by sum_fouls;
 
 
-#Q32 Write an SQL query to show average MRP by Outlet_Size and order it in descending order.
-SELECT  Outlet_Size, avg(Item_MRP) Average_MRP  
-FROM Grocery_Store  
-GROUP BY Outlet_Size order by Average_MRP desc;
+#46.  total number of games for each group and arrange it in descending order by games.
+select td.`group`, sum(gs.games) as sum_games 
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by sum_games desc;
 
-#Q33 Write an SQL query to Average MRP by Outlet_Type and ordered in ascending order. 
-SELECT  Outlet_Type, avg(Item_MRP)Average_MRP  
-FROM Grocery_Store  
-GROUP BY Outlet_Type order by Average_MRP asc;
+#47.  total number of players_used for each group and arrange it in ascending order by players_used.
+select td.`group`, sum(gs.players_used) as sum_players_used 
+from skysports2 td inner join skysports gs
+on td.team=gs.team
+group by td.`group` 
+order by sum_players_used;
 
-#Q34 Write an SQL query to show maximum MRP by Outlet_Type
-SELECT  Outlet_Type, max(Item_MRP)Max_MRP  
-FROM Grocery_Store  
-GROUP BY Outlet_Type order by Max_MRP asc;
+#48.  total number of offsides for each group and arrange it in ascending order by offsides.
+select td.`group`, sum(gs.offsides) as sum_offsides 
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by sum_offsides;
 
-#Q35 Write an SQL query to show maximum Item_Weight by Item_Type  
-SELECT Item_Type , max(Item_Weight)max_weight 
-FROM Grocery_Store  
-GROUP BY Item_Type  
-ORDER BY max_weight DESC;
+#49.  average passes_pct for each group and arrange it in descending order by passes_pct.
+select td.`group`, avg(gs.passes_pct) as avg_passes_pct
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by avg_passes_pct desc;
 
-#Q36 Write an SQL query to show maximum Item_Weight by Outlet_Establishment_Year 
-SELECT Outlet_Establishment_Year , max(Item_Weight) max_weight  
-FROM Grocery_Store  
-GROUP BY Outlet_Establishment_Year  
-ORDER BY max_weight asc; 
-
-#Q37 Write an SQL query to show minimum Item_Weight by Outlet_Type  
-SELECT Outlet_Type , min(Item_Weight)min_weight  
-FROM Grocery_Store  
-GROUP BY Outlet_Type  
-ORDER BY min_weight desc; 
-
-#Q38 Write an SQL query to show average Item_Weight by Outlet_Location_Type and arrange it by descending order
-SELECT Outlet_Location_Type , avg(Item_Weight) Average_weight  
-FROM Grocery_Store  
-GROUP BY Outlet_Location_Type  
-ORDER BY Average_weight desc;
-
-
-#Q39 Write an SQL query to show maximum Item_Outlet_Sales by Item_Type
-SELECT  Item_Type, Max(Item_Outlet_Sales)Max_sales  
-FROM Grocery_Store  
-GROUP BY Item_Type; 
-
-#Q40 Write an SQL query to show minimum Item_Outlet_Sales by Item_Type 
-SELECT  Item_Type, min(Item_Outlet_Sales)Min_sales  
-FROM Grocery_Store  
-GROUP BY Item_Type;
-
-#Q41 Write an SQL query to show minimum Item_Outlet_Sales by Outlet_Establishment_Year 
-SELECT  Outlet_Establishment_Year, min(Item_Outlet_Sales) Min_sales  
-FROM Grocery_Store  
-GROUP BY Outlet_Establishment_Year order by Min_sales desc;
-
-#Q42 Write an SQL query to show maximum Item_Outlet_Sales by Outlet_Establishment_Year and order it by descending order
-SELECT  Outlet_Establishment_Year,  Max(Item_Outlet_Sales) Max_sales 
-FROM Grocery_Store
-GROUP BY Outlet_Establishment_Year order by Max_sales desc;
-
-#Q43 Write an SQL query to show average Item_Outlet_Sales by Outlet_Size and order it it descending order
-SELECT  Outlet_Size, avg(Item_Outlet_Sales)Average_sales  
-FROM Grocery_Store  
-GROUP BY Outlet_Size order by Average_sales desc;
-
-#Q44 Write an SQL query to show average Item_Outlet_Sales by Outlet_Type
-SELECT  Outlet_Type, avg(Item_Outlet_Sales)Average_sales  
-FROM Grocery_Store  
-GROUP BY Outlet_Type order by Average_sales asc;
-
-#Q45 Write an SQL query to show maximum Item_Outlet_Sales by Outlet_Type 
-SELECT  Outlet_Type, max(Item_Outlet_Sales)Max_sales  
-FROM Grocery_Store  
-GROUP BY Outlet_Type order by Max_sales asc;
-
-#Q46 Write an SQL query to show total Item_Outlet_Sales by Item_Type 
-select Item_Type, sum(Item_Outlet_Sales) total_sales
-from Grocery_Store 
-group by Item_Type 
-order by total_sales desc;
-
-#Q47 Write an SQL query to show total Item_Outlet_Sales by Item_Fat_Content 
-select Item_Fat_Content, sum(Item_Outlet_Sales)total_sales
-from Grocery_Store 
-group by Item_Fat_Content 
-order by total_sales desc;
-
-#Q48 Write an SQL query to show maximum Item_Visibility by Item_Type
-select Item_Type, Max(Item_Visibility)max_visibility
-from Grocery_Store 
-group by Item_Type 
-order by max_visibility desc; 
-
-#Q49 Write an SQL query to show Minimum Item_Visibility by Item_Type 
-select Item_Type, Min(Item_Visibility)min_visibility
-from Grocery_Store 
-group by Item_Type 
-order by min_visibility desc; 
-
-#Q50 Write an SQL query to show total Item_Outlet_Sales by Item_Type but only WHERE Outlet_Location_Type is Tier 1 
-select Item_Type, sum(Item_Outlet_Sales)Total_sales
-from Grocery_Store where Outlet_Location_Type = 'Tier 1'
-group by Item_Type 
-order by Total_sales desc; 
-
-#Q51 Write an SQL query to show total Item_Outlet_Sales by Item_Type WHERE Item_Fat_Content is ONLY Low Fat & LF
-select Item_Type, sum(Item_Outlet_Sales)Total_sales
-from Grocery_Store where Item_Fat_Content in ('Low Fat', 'LF')
-group by Item_Type 
-order by Total_sales desc; 
-
+#50.  average goals_per90 for each group and arrange it in ascending order by goals_per90.
+select td.`group`, avg(gs.goals_per90) as avg_goals_per90
+from skysports2 td inner join skysports gs 
+on td.team=gs.team
+group by td.`group` 
+order by avg_goals_per90;
